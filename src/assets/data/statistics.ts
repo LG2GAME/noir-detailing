9c86b60fdca2d3b0601968fca00116a7afe1cafa
LG2GAME <lg2game6@gmail.com>
2026-08-28 19:53:52 +0200

    feat(statistics): add statistics data

---
+++ src/assets/data/statistics.ts
@@ -0,0 +1,18 @@
+export const statistics: { value: string; label: string }[] = [
+  {
+    value: '98%',
+    label: 'Customer satisfaction'
+  },
+  {
+    value: '100%',
+    label: 'Attention to detail'
+  },
+  {
+    value: '15+',
+    label: 'Years of experience'
+  },
+  {
+    value: '1000+',
+    label: 'Cars transformed'
+  }
+]
