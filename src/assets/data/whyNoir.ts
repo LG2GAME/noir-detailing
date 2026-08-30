d33535060405221561c06ca3cc50763617883c63
LG2GAME <lg2game6@gmail.com>
2026-08-30 18:48:31 +0200

    feat(services): add why noir content

---
+++ src/assets/data/whyNoir.ts
@@ -0,0 +1,20 @@
+export const whyNoir: { title: string; description: string; link: string }[] = [
+  {
+    title: 'Attention To Detail',
+    description:
+      'Every vehicle receives meticulous attention, refined techniques and uncompromising quality.',
+    link: '/about-us'
+  },
+  {
+    title: 'Premium Products',
+    description:
+      'We use advanced solutions designed to protect, enhance and preserve your vehicle.',
+    link: '/about-us'
+  },
+  {
+    title: 'Personal Approach',
+    description:
+      'Every project is carefully tailored to match your vehicle’s unique needs.',
+    link: '/about-us'
+  }
+]
