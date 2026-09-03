15bcec1ea0434209861b31a600b9307743dfd545
LG2GAME <lg2game6@gmail.com>
2026-09-03 12:02:12 +0200

    feat(assets): add icons to whyNoir

--- src/assets/data/whyNoir.ts
+++ src/assets/data/whyNoir.ts
@@ -1,17 +1,27 @@
-export const whyNoir: { title: string; description: string; link: string }[] = [
+import cube from '../icons/cube.svg'
+
+export const whyNoir: {
+  icon: { src: string; alt: string }
+  title: string
+  description: string
+  link: string
+}[] = [
   {
+    icon: { src: cube.src, alt: 'Attention To Detail' },
     title: 'Attention To Detail',
     description:
       'Every vehicle receives meticulous attention, refined techniques and uncompromising quality.',
     link: '/about-us'
   },
   {
+    icon: { src: cube.src, alt: 'Premium Products' },
     title: 'Premium Products',
     description:
       'We use advanced solutions designed to protect, enhance and preserve your vehicle.',
     link: '/about-us'
   },
   {
+    icon: { src: cube.src, alt: 'Personal Approach' },
     title: 'Personal Approach',
     description:
       'Every project is carefully tailored to match your vehicle’s unique needs.',
