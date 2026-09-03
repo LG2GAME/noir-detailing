2f5e0c63853df0af5641cc8c49591ef3174703a8
LG2GAME <lg2game6@gmail.com>
2026-09-03 14:53:14 +0200

    feat(components): add before/after slider

---
+++ src/components/ui/makeoverGallery/BeforeAfterComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { VueCompareImage } from 'vue3-compare-image'
+
+import beforeImage from '@/assets/images/gallery_after_1.webp'
+import afterImage from '@/assets/images/gallery_before_1.webp'
+</script>
+
+<template>
+  <VueCompareImage
+    :left-image="beforeImage.src"
+    :right-image="afterImage.src"
+    class="makeoverSlider"
+  />
+</template>
+
+<style scoped lang="scss">
+.makeoverSlider {
+  max-height: 100vh;
+}
+</style>
